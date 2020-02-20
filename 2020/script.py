24d9dc755f881fb0e97152cb847f29a2d25b16ad
Tib612 <thibaut-c@hotmail.com>
2020-02-20 14:14:57 -0500

    done2

--- 2020/script.py
+++ 2020/script.py
@@ -8,17 +8,22 @@ class Library:
 		self.speed = speed
 		self.books = books
 		self.processedBooks = []
-		print(f"Library created with {nbLivre} {sign} {speed} {books}")
+		#print(f"Library created with {nbLivre} {sign} {speed} {books}")
 
 def main():
 	filename="a_example"
+	#filename="b_read_on"
+	#filename="c_incunabula"
+	#filename="d_tough_choices"
+	#filename="e_so_many_books"
+	#filename="f_libraries_of_the_world"
 	print(f"reading from {filename}.in and exporting to {filename}.out")
 
 	with open(filename+'.txt',"r") as i, open(filename+'.out',"w") as o:
 		nbLivreTot, nbBibli, nbJour = [int(s) for s in i.readline().split(" ")]
 		scores = [int(s) for s in i.readline().split(" ")]
 
-		print(f"context is {nbLivreTot} {nbBibli} {nbJour} {scores}")
+		#print(f"context is {nbLivreTot} {nbBibli} {nbJour} {scores}")
 
 		libs = []  # librairies lue
 		for id in range(nbBibli):
@@ -31,7 +36,7 @@ def main():
 		librairies = [] # processed orderd by sign up
 
 		librairies = libs
-		librairies.sort(key=lambda e : e.speed)
+		librairies.sort(key=lambda e : e.speed,reverse=True)
 		for l in librairies:
 			l.processedBooks = l.books
 
