a70ce15be978f3545d0d650e768d6cc891a5635d
Tib612 <thibaut-c@hotmail.com>
2020-02-20 14:05:10 -0500

    done

--- 2020/script.py
+++ 2020/script.py
@@ -1,40 +1,45 @@
 import sys
 
 class Library:
-	def __init__(self, id, nbL, sign, speed, books):
+	def __init__(self, id, nbLivre, sign, speed, books):
 		self.id = id
-		self.nbL = nbL
+		self.nbLivre = nbLivre
 		self.sign = sign
 		self.speed = speed
 		self.books = books
 		self.processedBooks = []
-		print(f"Library created with {nbL} {sign} {speed} {books}")
+		print(f"Library created with {nbLivre} {sign} {speed} {books}")
 
 def main():
 	filename="a_example"
 	print(f"reading from {filename}.in and exporting to {filename}.out")
 
 	with open(filename+'.txt',"r") as i, open(filename+'.out',"w") as o:
-		nbL, nbB, nbJ = [int(s) for s in i.readline().split(" ")]
+		nbLivreTot, nbBibli, nbJour = [int(s) for s in i.readline().split(" ")]
 		scores = [int(s) for s in i.readline().split(" ")]
 
-		print(f"context is {nbL} {nbB} {nbJ} {scores}")
+		print(f"context is {nbLivreTot} {nbBibli} {nbJour} {scores}")
 
-		libs = []
-		for id in range(nbB):
-			nbL, sign, speed = [int(s) for s in i.readline().split(" ")]
+		libs = []  # librairies lue
+		for id in range(nbBibli):
+			nbLivre, sign, speed = [int(s) for s in i.readline().split(" ")]
 			books = [int(s) for s in i.readline().split(" ")]
-			libs.append(Library(id, nbL, sign, speed, books))
+			libs.append(Library(id, nbLivre, sign, speed, books))
 		
 
 		# code her
-		librairies = [] # orderd by sign up
+		librairies = [] # processed orderd by sign up
+
+		librairies = libs
+		librairies.sort(key=lambda e : e.speed)
+		for l in librairies:
+			l.processedBooks = l.books
 
 
-		o.write(str(len(librairies)))
+		o.write(str(len(librairies))+"\n")
 		for lib in librairies:
-			o.write(f"{lib.id} {len(lib.processedBooks)}")
-			o.write(' '.join(lib.processedBooks))
+			o.write(f"{lib.id} {len(lib.processedBooks)}\n")
+			o.write(' '.join(map(str,lib.processedBooks))+"\n")
 
 
 if __name__ == "__main__":
