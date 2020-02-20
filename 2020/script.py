e54f2189e6321d973f621dec9a7f7a88a92b89f4
Tib612 <thibaut-c@hotmail.com>
2020-02-20 12:49:35 -0500

    adding question and data

--- 2020/script.py
+++ 2020/script.py
@@ -1,6 +1,7 @@
 import sys
 
 filename="a_example"
+print(f"reading from {filename}.in and exporting to {filename}.out")
 
 with open(filename+'.in',"r") as i, open(filename+'.out',"w") as o:
 	m , n = [int(s) for s in i.readline().split(" ")]
